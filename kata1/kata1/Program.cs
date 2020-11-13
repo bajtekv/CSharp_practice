3963438f64ffc1402f496905b7acb8514685d52d
bajtekv <56979311+bajtekv@users.noreply.github.com>
2020-11-13 14:15:55 +0100

    Split to array
    Added new funtion called SplitString(). It splits a string into pair of two characters. These characters are added to an array. If the string contains an odd number of chars, missing char is replaced by "_".

---
+++ kata1/kata1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace kata1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", SplitString("ahojda")));
+            Console.WriteLine("[{0}]", string.Join(", ", SplitString("liska")));
+            Console.ReadLine();
+        }
+        public static string[] SplitString(string str)
+        {
+            int a = 0;
+            bool odd = false;
+            if (str.Length % 2 == 0)
+            {
+                a = str.Length / 2;
+            }
+            else
+            {
+                a = (str.Length + 1) / 2;
+                odd = true;
+            }
+            string[] resultArray = new string[a];
+            int k = 0;
+            string tmp = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                tmp += str[i];
+                if (i % 2 != 0)
+                {
+                    resultArray[k] = tmp;
+                    k += 1;
+                    tmp = "";
+                }
+                if (i == str.Length - 1 && odd)
+                {
+                    resultArray[k] += str[i];
+                    resultArray[k] += "_";
+                }
+            }
+            return resultArray;
+        }
+    }
+}
