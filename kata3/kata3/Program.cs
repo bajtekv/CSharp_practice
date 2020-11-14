6a1f05c1f11edb3fd5ac11b5204b0be77f836a37
bajtekv <56979311+bajtekv@users.noreply.github.com>
2020-11-14 23:27:25 +0100

    new kata

---
+++ kata3/kata3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace kata3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Accumul("abdcefghijk"));
+            Console.ReadKey(true);
+        }
+        public static string Accumul(string s)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0)
+                    {
+                        result += s[i].ToString().ToUpper();
+                    }
+                    else
+                    {
+                        result += s[i];
+                    }
+                }
+                result += "-";
+            }
+            result = result.Remove(result.LastIndexOf("-"));
+            return result;
+        }
+    }
+}
