4b9f2177e0afbff667215d2570c28f34cbe0114a
bajtekv <56979311+bajtekv@users.noreply.github.com>
2020-11-14 22:30:48 +0100

    PhoneNumber
    Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
    ex.:
    Kata.CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"

---
+++ kata2/kata2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace kata2
+{
+    /*  Write a function that accepts an array of 
+     *  10 integers (between 0 and 9), that returns a 
+     *  string of those numbers in the form of a phone number.
+     *  Kata.CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+     */
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(CreatePhoneNumber(new int[] { 1, 3, 1, 0, 1, 3, 7, 3, 9, 1 }));
+            Console.ReadLine();
+        }
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            if (numbers.Length != 10)
+            {
+                return null;
+            }
+            string formattedNumber = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i == 0)
+                {
+                    formattedNumber += "(";
+                    formattedNumber += numbers[i];
+                }
+                else if (i == 3)
+                {
+                    formattedNumber += ") ";
+                    formattedNumber += numbers[i];
+                }
+                else if (i == 6)
+                {
+                    formattedNumber += "-";
+                    formattedNumber += numbers[i];
+                }
+                else
+                {
+                    formattedNumber += numbers[i];
+                }
+            }
+            return formattedNumber;
+        }
+        public static string CreateNumber2(int[] numbers) //great code by cinq
+        {
+            return String.Format("{0:(000) 000-0000}", ulong.Parse(string.Join("", numbers)));
+        }
+    }
+}
